586f8e7aabc1cad51b07874dcce13320349160a0
Muhammad Zaid <bc220407371mza@vu.edu.pk>
2025-10-30 15:17:11 +0500

    Edges are working but graph is unweighted

--- main.py
+++ main.py
@@ -1,3 +1,36 @@
-import engine
+# Getting random numbers from 1 to 20
+# import random
+# upper_limit = 20
+# print(int(random.random() * 20) + 1)
 
-game = engine.Engine((800, 600))
+# 1D array to 2D array
+# arr = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+# rows = 3
+# columns = 3
+#
+# arr_2 = [0] * columns
+# arr_3 = [arr_2] * rows
+#
+# arr_3[1][2] = 69
+#
+# print(arr_3[1][2])
+# print(arr_3[0][0])
+
+import weighted_graph
+Test = weighted_graph.Weighted_Graph(3, 3)
+
+# edge_1 = {1, 2}
+# edge_2 = {2, 1}
+#
+# if (edge_1 == edge_2):
+#     print("Both are same edges")
+# else:
+#     print("Both are not same edges")
+
+# edge = {1, 2}
+# x = 1
+# y = 2
+# if (x in edge):
+#     print(x, " is in the edge")
+# else:
+#     print(x, " is not in the edge")
--- weighted_graph.py
+++ weighted_graph.py
@@ -1,3 +1,5 @@
+import random
+
 def get_vertices(rows, columns):
     vertices = []
     id = 0
@@ -6,47 +8,62 @@ def get_vertices(rows, columns):
         id = id + 1
     return vertices
 
-def get_edges(rows, columns, vertices):
+def get_grid(rows, columns, vertices):
     # Make Grid
-    row = [0] * columns
-    grid = [row] * rows
-
-    # NOTE: DEBUG - Checking Whole Vertex Map
-    print(vertices)
+    grid = [[0] * columns for _ in range(rows)]
 
     # Populate Grid
     for i in range(rows * columns):
-        row_index = int(i / rows)
+        row_index = i // columns
         column_index = i % columns
-
-        # NOTE: DEBUG INFORMATION
-        # WARN: GRID IS NOT CORRECTLY POPULATED, SOMETHING IS GOOPHI FOR SURE
-        print("Loop Index: ", i)
-        print("Row Index: ", row_index)
-        print("Columns Index: ", column_index)
-        print("Vertex ID: ", vertices[i])
         grid[row_index][column_index] = vertices[i]
 
-    # Populate Grid
-    # for i in range(rows):
-    #     for j in range(columns):
-    #         grid[i][j] = vertices[j + (columns * i)]
-
-    # debug
-    print("Grid: ", grid)
     return grid
+
+def get_edges(rows, columns, grid):
+    edges = []
+    for i in range(rows):
+        for j in range(columns):
+            row_index = i
+            column_index = j
+
+            # Up
+            if(not (row_index - 1 < 0)):
+                pair = {grid[i][j], grid[row_index - 1][j]}
+                if (not pair in edges):
+                    edges.append(pair)
+
+            # Right
+            if (not (column_index + 1 >= columns)):
+                pair = {grid[i][j], grid[i][j + 1]}
+                if (not pair in edges):
+                    edges.append(pair)
+
+            # Down
+            if(not (row_index + 1 >= rows)):
+                pair = {grid[i][j], grid[row_index + 1][j]}
+                if (not pair in edges):
+                    edges.append(pair)
+
+            # Left
+            if (not (column_index - 1 < 0)):
+                pair = {grid[i][j], grid[i][j - 1]}
+                if (not pair in edges):
+                    edges.append(pair)
+
+    print(len(edges))
+    return edges
 
 class Weighted_Graph:
     def __init__(self, rows, columns):
-        self.vertex = get_vertices(rows, columns)
-        self.grid = get_edges(rows, columns, self.vertex)
-
-    # Edge = ((v1, v2), weight)
+        self.vertices = get_vertices(rows, columns)
+        self.grid = get_grid(rows, columns, self.vertices)
+        self.edges = get_edges(rows, columns, self.grid)
 
     # Debug
     def print_vertex_count(self):
-        print(self.vertex)
+        print(self.vertices)
 
-    # def print_grid(self):
-    #     print(self.grid)
-    #
+    def print_grid(self):
+        print(self.grid)
+
