4ec6415626f460429e1097a4c981e68bffd18a28
Muhammad Zaid <bc220407371mza@vu.edu.pk>
2025-12-21 16:13:12 +0500

    Added: Graph and Prim algorithms definitions

--- prototype/algorithms/prims_algo.py
+++ prototype/algorithms/prims_algo.py
@@ -1,77 +1,22 @@
-# Source: https://www.geeksforgeeks.org/dsa/prims-minimum-spanning-tree-mst-greedy-algo-5/
+# Source: https://www.w3schools.com/dsa/dsa_algo_mst_prim.php
 
-# A Python3 program for 
-# Prim's Minimum Spanning Tree (MST) algorithm.
-# The program is for adjacency matrix 
-# representation of the graph
+def prims_algorithm(graph):
+    in_mst = [False] * graph.size
+    key_values = [float('inf')] * graph.size
+    parents = [-1] * graph.size
 
-# Library for INT_MAX
-import sys
+    key_values[0] = 0  # Starting vertex
 
-class Graph():
-    def __init__(self, vertices):
-        self.V = vertices
-        self.graph = [[0 for column in range(vertices)]
-                      for row in range(vertices)]
+    print("Edge \tWeight")
+    for _ in range(graph.size):
+        u = min((v for v in range(graph.size) if not in_mst[v]), key=lambda v: key_values[v])
 
-    # A utility function to print 
-    # the constructed MST stored in parent[]
-    def printMST(self, parent):
-        print("Edge \tWeight")
-        for i in range(1, self.V):
-            print(parent[i], "-", i, "\t", self.graph[parent[i]][i])
+        in_mst[u] = True
 
-    # A utility function to find the vertex with
-    # minimum distance value, from the set of vertices
-    # not yet included in shortest path tree
-    def minKey(self, key, mstSet):
+        if parents[u] != -1:  # Skip printing for the first vertex since it has no parent
+            print(f"{graph.vertex_data[parents[u]]}-{graph.vertex_data[u]} \t{graph.adj_matrix[u][parents[u]]}")
 
-        # Initialize min value
-        min = sys.maxsize
-
-        for v in range(self.V):
-            if key[v] < min and mstSet[v] == False:
-                min = key[v]
-                min_index = v
-
-        return min_index
-
-    # Function to construct and print MST for a graph
-    # represented using adjacency matrix representation
-    def primMST(self):
-
-        # Key values used to pick minimum weight edge in cut
-        key = [sys.maxsize] * self.V
-        parent = [None] * self.V  # Array to store constructed MST
-        # Make key 0 so that this vertex is picked as first vertex
-        key[0] = 0
-        mstSet = [False] * self.V
-
-        parent[0] = -1  # First node is always the root of
-
-        for cout in range(self.V):
-
-            # Pick the minimum distance vertex from
-            # the set of vertices not yet processed.
-            # u is always equal to src in first iteration
-            u = self.minKey(key, mstSet)
-
-            # Put the minimum distance vertex in
-            # the shortest path tree
-            mstSet[u] = True
-
-            # Update dist value of the adjacent vertices
-            # of the picked vertex only if the current
-            # distance is greater than new distance and
-            # the vertex in not in the shortest path tree
-            for v in range(self.V):
-
-                # graph[u][v] is non zero only for adjacent vertices of m
-                # mstSet[v] is false for vertices not yet included in MST
-                # Update the key only if graph[u][v] is smaller than key[v]
-                if self.graph[u][v] > 0 and mstSet[v] == False \
-                and key[v] > self.graph[u][v]:
-                    key[v] = self.graph[u][v]
-                    parent[v] = u
-
-        self.printMST(parent)
+            for v in range(graph.size):
+                if 0 < graph.adj_matrix[u][v] < key_values[v] and not in_mst[v]:
+                    key_values[v] = graph.adj_matrix[u][v]
+                    parents[v] = u
