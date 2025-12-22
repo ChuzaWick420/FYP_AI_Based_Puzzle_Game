7e0277c1b995fd3ac5f15817e3bcb8dd529d6c74
Muhammad Zaid <bc220407371mza@vu.edu.pk>
2025-12-22 13:27:11 +0500

    Test verified: Level_dependency

--- tests/Level_dependency.py
+++ tests/Level_dependency.py
@@ -1,5 +1,16 @@
 from prototype.algorithms.prims_algo import prims_algorithm
 from prototype.services.populate_graph import populate_graph
-from prototype.entities.Graph import Graph
-from prototype.entities.maze_type import MazeType
+from prototype.services.level_to_graph import level_to_graph
+import math
 
+level = 1
+
+graph = level_to_graph(level)
+print("Graph size: ", int(math.sqrt(graph.size)), "x", int(math.sqrt(graph.size)))
+print("Graph after creation: ", graph.adj_matrix)
+
+populate_graph(graph, graph.size)
+print("Graph after population: ", graph.adj_matrix)
+
+prims_algorithm(graph)
+print("Graph after prim's algorithm: ", graph.adj_matrix)
