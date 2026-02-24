bb9606e83b9f4691ebc0766c1b30ba3ef55a2ee4
Muhammad Zaid <bc220407371mza@vu.edu.pk>
2026-02-24 18:49:31 +0500

    Added: Button.isClicked()
    return true when clicked by cursor, otherwise returns false

--- prototype/entities/Button.py
+++ prototype/entities/Button.py
@@ -13,3 +13,18 @@ class Button:
     def updatePosition(self, pos):
         self.position = pos
         self.textRect.center = self.position
+
+    def isClicked(self):
+        flag = False
+
+        mouse_pos = pygame.mouse.get_pos()
+
+        horizontal_left  = mouse_pos[0] >= self.position[0] - self.textRect.width // 2
+        horizontal_right = mouse_pos[0] <= self.position[0] + self.textRect.width // 2
+        vertical_up      = mouse_pos[1] >= self.position[1] - self.textRect.height // 2
+        vertical_down    = mouse_pos[1] <= self.position[1] + self.textRect.height // 2
+
+        if (horizontal_left and horizontal_right and vertical_up and vertical_down):
+            flag = True
+
+        return flag
