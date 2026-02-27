82bb0b84af8c898e12ef4c5e7ed07aa686e0bbec
Muhammad Zaid <bc220407371mza@vu.edu.pk>
2026-02-27 11:32:39 +0500

    Added: SystemEvents

---
+++ prototype/data/SystemEvents.py
@@ -0,0 +1,7 @@
+class SystemEvents:
+    # NOTE: Format: xxff - xx determines the type of event where ff is the id.
+    #               xx = 10 for input events
+    #               xx = 11 for window events
+
+    MOUSE_CLICK    = 1001
+    TERMINATE_GAME = 1101
