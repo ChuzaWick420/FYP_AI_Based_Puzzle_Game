158a94e0c09f923194b4c9bb746d83d54a8030fe
Muhammad Zaid <bc220407371mza@vu.edu.pk>
2026-02-27 11:37:48 +0500

    Added: InputHandler

---
+++ prototype/presentation_layer/InputHandler.py
@@ -0,0 +1,18 @@
+from prototype.data.SystemEvents import SystemEvents
+import pygame
+
+class InputHandler:
+    def __init__(self):
+        return
+
+    def processEvents(self):
+        system_events = []
+
+        for event in pygame.event.get():
+            if event.type == pygame.quit():
+                system_events.append(SystemEvents.TERMINATE_GAME)
+
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                system_events.append(SystemEvents.MOUSE_CLICK)
+
+        return system_events
