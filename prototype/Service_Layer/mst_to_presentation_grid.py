900cd7217e14f02ce68bf2c70b6f67972007305c
Muhammad Zaid <bc220407371mza@vu.edu.pk>
2026-04-09 09:56:31 +0500

    Added padding to maze

--- prototype/Service_Layer/mst_to_presentation_grid.py
+++ prototype/Service_Layer/mst_to_presentation_grid.py
@@ -1,8 +1,28 @@
 # MST to grid
 def mst_to_presentation_grid(graph):
     # NOTE: Nodes + Edges
-    grid_width = (graph.width) + (graph.width - 1) 
+    nodes_amount = graph.width
+    edges_amount = graph.width - 1
+    padding = 1 + 1
+    grid_width = nodes_amount + edges_amount + padding
+
     presentation_grid = [[0] * grid_width for _ in range(grid_width)]
+
+    # NOTE: Goes through padding (up and down)
+    for i in range(0, grid_width):
+        presentation_grid[0][i] = 0
+        presentation_grid[grid_width - 1][i] = 0
+
+    # NOTE: Goes through padding (right and left)
+    for i in range(0, grid_width):
+        presentation_grid[i][0] = 0
+        presentation_grid[i][grid_width - 1] = 0
+
+    # TODO: Create openings within padding
+    presentation_grid[1][0] = 1
+    presentation_grid[grid_width - 2][grid_width - 1] = 1
+
+    offsets = (1, 1)
 
     # NOTE: Goes through each vertex
     for node_index in range(0, graph.total_nodes):
@@ -14,12 +34,12 @@ def mst_to_presentation_grid(graph):
         right_allowed = x + 1 < graph.width and graph.adj_matrix[node_index][graph.coordinates_to_index(x + 1, y)] != 0
         left_allowed  = x - 1 >= 0          and graph.adj_matrix[node_index][graph.coordinates_to_index(x - 1, y)] != 0
 
-        if (up_allowed):    presentation_grid[(y * 2) - 1][x * 2] = 1 
-        if (down_allowed):  presentation_grid[(y * 2) + 1][x * 2] = 1 
-        if (right_allowed): presentation_grid[y * 2][(x * 2) + 1] = 1 
-        if (left_allowed):  presentation_grid[y * 2][(x * 2) - 1] = 1 
+        if (up_allowed):    presentation_grid[(y * 2) - 1 + offsets[1]][x * 2 + offsets[0]] = 1 
+        if (down_allowed):  presentation_grid[(y * 2) + 1 + offsets[1]][x * 2 + offsets[0]] = 1 
+        if (right_allowed): presentation_grid[y * 2 + offsets[0]][(x * 2) + 1 + offsets[0]] = 1 
+        if (left_allowed):  presentation_grid[y * 2 + offsets[0]][(x * 2) - 1 + offsets[0]] = 1 
 
         # NOTE: Vertices
-        presentation_grid[y * 2][x * 2] = 1
+        presentation_grid[y * 2 + offsets[1]][x * 2 + offsets[0]] = 1
 
     return presentation_grid
