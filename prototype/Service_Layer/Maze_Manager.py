1eb28e7d9ebc2d3d5d15d747a3a7e02b3a45d54d
Muhammad Zaid <bc220407371mza@vu.edu.pk>
2026-04-12 19:03:38 +0500

    Moved: grip map generation into maze manager

--- prototype/Service_Layer/Maze_Manager.py
+++ prototype/Service_Layer/Maze_Manager.py
@@ -4,18 +4,17 @@ from prototype.Data_Layer.CellTypes import CellTypes
 from prototype.Domain_Logic_Layer.algorithms.a_star_search_algo import a_star_search
 from prototype.Domain_Logic_Layer.algorithms.prims_algo import prims_algorithm
 from prototype.Domain_Logic_Layer.entities.Graph import Graph
-from prototype.Domain_Logic_Layer.entities.Grid_Map import Grid_Map
 from prototype.Service_Layer.spawn_entities import spawn_entities
 
 
 class Maze_Manager:
     def __init__(self):
-        self.graph = Graph(16 * 16)
+        size = 16 * 16
 
+        self.graph = Graph(size)
         mst = prims_algorithm(self.graph)
         self.graph.adj_matrix = mst
-
-        self.maze_map = Grid_Map(self.graph).get_map()
+        self.maze_map = self.generate_grid_map()
 
         size = len(self.maze_map)
 
@@ -34,6 +33,52 @@ class Maze_Manager:
         #
         # for row in self.maze_map:
         #     print(row)
+
+    def generate_grid_map(self):
+        # NOTE: Nodes + Edges
+        nodes_amount = self.graph.width
+        edges_amount = self.graph.width - 1
+        padding = 1 + 1
+        grid_width = nodes_amount + edges_amount + padding
+
+        map = [[0] * grid_width for _ in range(grid_width)]
+
+        # NOTE: Goes through padding (up and down)
+        for i in range(0, grid_width):
+            map[0][i] = CellTypes.WALL.value
+            map[grid_width - 1][i] = CellTypes.WALL.value
+
+        # NOTE: Goes through padding (right and left)
+        for i in range(0, grid_width):
+            map[i][0] = CellTypes.WALL.value
+            map[i][grid_width - 1] = CellTypes.WALL.value
+
+        # NOTE: Create openings within padding
+        map[1][0] = CellTypes.PATH.value
+        map[grid_width - 2][grid_width - 1] = CellTypes.PATH.value
+
+        offsets = (1, 1)
+
+        # NOTE: Goes through each vertex
+        for node_index in range(0, self.graph.total_nodes):
+
+            (x, y) = self.graph.index_to_coordinates(node_index)
+
+            up_allowed    = y - 1 >= 0          and self.graph.adj_matrix[node_index][self.graph.coordinates_to_index(x, y - 1)] != 0
+            down_allowed  = y + 1 < self.graph.width and self.graph.adj_matrix[node_index][self.graph.coordinates_to_index(x, y + 1)] != 0
+            right_allowed = x + 1 < self.graph.width and self.graph.adj_matrix[node_index][self.graph.coordinates_to_index(x + 1, y)] != 0
+            left_allowed  = x - 1 >= 0          and self.graph.adj_matrix[node_index][self.graph.coordinates_to_index(x - 1, y)] != 0
+
+            if (up_allowed):    map[(y * 2) - 1 + offsets[1]][x * 2 + offsets[0]] = CellTypes.PATH.value
+            if (down_allowed):  map[(y * 2) + 1 + offsets[1]][x * 2 + offsets[0]] = CellTypes.PATH.value
+            if (right_allowed): map[y * 2 + offsets[0]][(x * 2) + 1 + offsets[0]] = CellTypes.PATH.value
+            if (left_allowed):  map[y * 2 + offsets[0]][(x * 2) - 1 + offsets[0]] = CellTypes.PATH.value
+
+            # NOTE: Vertices
+            map[y * 2 + offsets[1]][x * 2 + offsets[0]] = CellTypes.PATH.value
+
+        return map
+
 
     def get_render_data(self):
         render_data = []
