3aff8fc177846d5825848026d26d6b19c774064b
Muhammad Zaid <bc220407371mza@vu.edu.pk>
2026-05-07 16:49:47 +0500

    Changed blinder's colors so it seems that walls have disappeared
    Closes #6, Closes #7, Closes #10

--- src/Data_Layer/CellTypes.py
+++ src/Data_Layer/CellTypes.py
@@ -5,7 +5,8 @@ class CellTypes:
     PLAYER         = {"value": 2, "color": (16, 230, 30)}
     AI             = {"value": 3, "color": (214, 34, 21)}
     PLAYER_AND_AI  = {"value": 4, "color": (189, 159, 13)}
-    BLINDER        = {"value": 5, "color": (179, 18, 50)}
+    # BLINDER        = {"value": 5, "color": (179, 18, 50)} # NOTE: Turn these on for debugging
+    BLINDER        = {"value": 5, "color": (0, 0, 0)}
     SOURCE         = {"value": 6, "color": (120, 250, 146)}
     GOAL           = {"value": 7, "color": (10, 57, 138)}
     POWERUP_REVEAL = {"value": 8, "color": (116, 7, 138)}
