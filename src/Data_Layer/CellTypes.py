e6f3d97ccd274d7a03a9674af9c776165ef00916
Muhammad Zaid <bc220407371mza@vu.edu.pk>
2026-05-11 05:42:28 +0500

    Changed Color Scheme for maze entities

--- src/Data_Layer/CellTypes.py
+++ src/Data_Layer/CellTypes.py
@@ -7,7 +7,7 @@ class CellTypes:
     PLAYER_AND_AI  = {"value": 4, "color": (189, 159, 13)}
     # BLINDER        = {"value": 5, "color": (179, 18, 50)} # NOTE: Turn these on for debugging
     BLINDER        = {"value": 5, "color": (0, 0, 0)}
-    SOURCE         = {"value": 6, "color": (120, 250, 146)}
-    GOAL           = {"value": 7, "color": (10, 57, 138)}
-    POWERUP_REVEAL = {"value": 8, "color": (116, 7, 138)}
-    POWERUP_SLOW   = {"value": 9, "color": (255, 120, 3)}
+    SOURCE         = {"value": 6, "color": (52, 103, 81)}
+    GOAL           = {"value": 7, "color": (200, 75, 49)}
+    POWERUP_REVEAL = {"value": 8, "color": (100, 13, 95)}
+    POWERUP_SLOW   = {"value": 9, "color": (234, 34, 100)}
