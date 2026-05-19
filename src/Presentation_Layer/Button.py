75ffc7ce320b76b009bf106655d93db58a9b821f
Muhammad Zaid <bc220407371mza@vu.edu.pk>
2026-05-19 10:29:58 +0500

    Converted public attributes to private
    Closes #11, #9

--- src/Presentation_Layer/Button.py
+++ src/Presentation_Layer/Button.py
@@ -5,53 +5,52 @@ from src.Presentation_Layer.TextElement import TextElement
 
 class Button:
     def __init__(self, text_str, input, pos = (0, 0)):
-        self.str = text_str
-        self.input = input
+        self.__str = text_str
+        self.__input = input
 
-        self.position = pos
-        self.width = 250
-        self.height = 0
-        self.font_size = 28
+        self.__position = pos
+        self.__width = 250
+        self.__font_size = 28
 
-        self.text = TextElement(self.str, Colors.TEXT, self.position)
+        self.text = TextElement(self.__str, Colors.TEXT, self.__position)
 
-        self.text.setSize(self.font_size)
+        self.text.setSize(self.__font_size)
 
         self.__hover_flag = False
         self.__active_flag = False
         self.__use_icon = False
 
-        self.btn_type = -1
+        self.__btn_type = -1
 
         # NOTE: Border
 
-        self.rect = pygame.Rect(0, 0, 0, 0)
-        self.padding = 16
-        self.thickness = 2
-        self.border_radius = 8
+        self.__rect = pygame.Rect(0, 0, 0, 0)
+        self.__padding = 16
+        self.__thickness = 2
+        self.__border_radius = 8
 
     def setInput(self, input):
-        self.input = input
+        self.__input = input
 
     def getInput(self):
-        return self.input
+        return self.__input
 
     def setType(self, btn_type):
-        self.btn_type = btn_type
+        self.__btn_type = btn_type
 
         reference = pygame.Rect(0, 0, 0, 0)
 
-        if (self.btn_type == ButtonType.TEXTUAL):
+        if (self.__btn_type == ButtonType.TEXTUAL):
             reference = self.text.getRect()
-            self.rect.width = self.width
+            self.__rect.width = self.__width
 
         else:
             reference = self.icon.get_rect()
-            reference.center = self.position
-            self.rect.width = reference.width + self.padding
+            reference.center = self.__position
+            self.__rect.width = reference.width + self.__padding
 
-        self.rect.height = reference.height + self.padding
-        self.rect.center = reference.center
+        self.__rect.height = reference.height + self.__padding
+        self.__rect.center = reference.center
 
     def loadIcon(self, path):
         image = pygame.image.load(path)
@@ -60,16 +59,16 @@ class Button:
         self.setType(ButtonType.VISUAL)
 
     def setPosition(self, pos):
-        self.position = pos
+        self.__position = pos
         self.text.setPosition(pos)
-        self.setType(self.btn_type)
+        self.setType(self.__btn_type)
 
     def isHovered(self):
         self.__hover_flag = False
 
         mouse_pos = pygame.mouse.get_pos()
 
-        if self.rect.collidepoint(mouse_pos):
+        if self.__rect.collidepoint(mouse_pos):
             self.__hover_flag = True
 
         return self.__hover_flag
@@ -85,13 +84,13 @@ class Button:
 
     def render(self, display):
         if self.__hover_flag == False:
-            pygame.draw.rect(display, Colors.BORDER, self.rect, self.thickness, self.border_radius)
+            pygame.draw.rect(display, Colors.BORDER, self.__rect, self.__thickness, self.__border_radius)
             self.text.setColor(Colors.TEXT)
         else:
-            pygame.draw.rect(display, Colors.HOVER, self.rect, self.thickness, self.border_radius)
+            pygame.draw.rect(display, Colors.HOVER, self.__rect, self.__thickness, self.__border_radius)
             self.text.setColor(Colors.HOVER)
 
         if (self.__use_icon):
-            display.blit(self.icon, (self.position[0] - 16, self.position[1] - 16))
+            display.blit(self.icon, (self.__position[0] - 16, self.__position[1] - 16))
         else:
             self.text.render(display)
