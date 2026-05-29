a198217bf2db47c6264f47cc42fb9f126a6e74e4
Muhammad Zaid <bc220407371mza@vu.edu.pk>
2026-05-29 14:57:48 +0500

    Made Playing screen members private

--- src/Presentation_Layer/PlayingScreen.py
+++ src/Presentation_Layer/PlayingScreen.py
@@ -1,7 +1,6 @@
 import pygame
 
 from src.Data_Layer import Global
-from src.Data_Layer.ButtonType import ButtonType
 from src.Data_Layer.StateInputs import StateInputs
 from src.Presentation_Layer.Block import Block
 from src.Presentation_Layer.Button import Button
@@ -10,36 +9,37 @@ from src.Presentation_Layer.TextElement import TextElement
 class PlayingScreen:
     def __init__(self):
         color = (176, 228, 204)
-        self.timer = TextElement("Timer: 00:00", color, (100, 50))
-        self.level_num = TextElement("Level: 1", color, (100, 100))
+        self.__timer = TextElement("Timer: 00:00", color, (100, 50))
+        self.__level_num = TextElement("Level: 1", color, (100, 100))
 
-        self.pause_button = Button("Pause", StateInputs.PAUSE, (Global.WINDOW_RESOLUTION[0] - 150, 100))
-        self.pause_button.loadIcon("assets/pause_button.png")
+        self.__pause_button = Button("Pause", StateInputs.PAUSE, (Global.WINDOW_RESOLUTION[0] - 150, 100))
+        self.__pause_button.loadIcon("assets/pause_button.png")
 
-        self.visual = pygame.sprite.Group()
+        self.__visual = pygame.sprite.Group()
 
     def setTimer(self, minutes, seconds):
-        self.timer.setText("Timer: {0:02}:{1:02}".format(minutes, seconds))
+        self.__timer.setText("Timer: {0:02}:{1:02}".format(minutes, seconds))
 
     def setLevel(self, level):
-        self.level_num.setText(f"Level: {str(level)}")
+        self.__level_num.setText(f"Level: {str(level)}")
 
     def update_maze(self, render_data):
-        self.visual = self.get_visual(render_data)
+        self.__visual = self.get_visual(render_data)
 
     def render(self, display):
-        self.timer.render(display)
-        self.level_num.render(display)
+        self.__timer.render(display)
+        self.__level_num.render(display)
 
-        self.pause_button.render(display)
+        self.__pause_button.render(display)
 
         # NOTE: Updates the visual and renders it
-        self.visual.draw(display)
+        self.__visual.draw(display)
 
-    # PERF: Need optimization, updates are really laggy
     def get_visual(self, render_data):
         maze_visual = pygame.sprite.Group()
 
+        # PERF: Need optimization, updates are really laggy.
+        #       Following 2 lines seems to be the source of lag.
         for color, size, pos in render_data:
             maze_visual.add(Block(color, size, pos))
 
